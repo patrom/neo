b3edbcb6e7a8783690300d46de90d822cef117e7
prombouts <patrik.rombouts@telenet.be>
2015-02-20 16:55:14 +0100

    minor scales removed

--- neo/src/neo/model/note/Scale.java
+++ neo/src/neo/model/note/Scale.java
@@ -7,11 +7,8 @@ public class Scale {
 	public static final int[] 
             CHROMATIC_SCALE = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
             MAJOR_SCALE = {0, 2, 4, 5, 7, 9, 11},
-            MINOR_SCALE = {0, 2, 3, 5, 7, 8, 10},
             HARMONIC_MINOR_SCALE = {0, 2, 3, 5, 7, 8, 11},
             MELODIC_MINOR_SCALE = {0, 2, 3, 5, 7, 8, 9, 10, 11}, // mix of ascend and descend
-            NATURAL_MINOR_SCALE = {0, 2, 3, 5, 7, 8, 10},
-            DIATONIC_MINOR_SCALE = {0, 2, 3, 5, 7, 8, 10},
             AEOLIAN_SCALE = {0, 2, 3, 5, 7, 8, 10},
             DORIAN_SCALE = {0, 2, 3, 5, 7, 9, 10},	
             LYDIAN_SCALE = {0, 2, 4, 6, 7, 9, 11},
